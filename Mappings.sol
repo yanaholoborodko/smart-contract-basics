9a8a4818fff98a0faa7f78b5bd9438a48f570cd7
Yana <holoborodkoyana97@gmail.com>
2020-11-09 10:46:42 +0100

    Creating a mapping and a get function for the data inside.

---
+++ Mappings.sol
@@ -0,0 +1,34 @@
+pragma solidity 0.5.12;
+
+contract Structs {
+
+    struct Person {
+        string name;
+        uint age;
+        bool adult;
+    }
+
+    mapping(address => Person) private people;
+
+    function createPerson(string memory name, uint age) public {
+        address creator = msg.sender;
+        
+        Person memory newPerson;
+        newPerson.name = name;
+        newPerson.age = age;
+        if(age > 18) {
+            newPerson.adult = true;
+        } else {
+            newPerson.adult = false;
+        }
+        // newPerson.adult = age > 18;
+
+        people[creator] = newPerson;
+
+    }
+
+    function getPerson() public view returns(string memory name, uint age, bool adult) {
+        return (people[msg.sender].name, people[msg.sender].age, people[msg.sender].adult);
+    }
+
+}
